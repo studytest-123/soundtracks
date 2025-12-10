8010368e918da37b53b2a25eaac89b693df99a47
unknown <studytest@web.de>
2025-12-10 22:00:19 +0100

    initial commit

---
+++ src/main/java/com/paf/soundtracks/SoundtracksApplication.java
@@ -0,0 +1,13 @@
+package com.paf.soundtracks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundtracksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundtracksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paf/soundtracks/SoundtracksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.soundtracks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundtracksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
